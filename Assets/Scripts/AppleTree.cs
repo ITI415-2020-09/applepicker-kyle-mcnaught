8dba9391b0d9b5bc16475aa91e60381aff694963
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-09-18 16:12:06 -0400

    Apple Tree movement
    The apple tree is now able to move from left to right

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -30,6 +30,15 @@ public class AppleTree : MonoBehaviour
     
     void Update()
     {
+        Vector3 pos = transform.position;                 
+        pos.x += speed * Time.deltaTime;                   
+        transform.position = pos;                         
+        
+         if ( pos.x < -leftAndRightEdge ) {                            
+           speed = Mathf.Abs(speed); // Move right                  
+       } else if ( pos.x > leftAndRightEdge ) {                     
+           speed = -Mathf.Abs(speed); // Move left                  
+       }
         
     }
 }
