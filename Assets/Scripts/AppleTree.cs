116d93d8f8408835d812a54dac9a6d2598a10732
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-09-18 16:00:28 -0400

    AppleTree Script setup
    AppleTree Script written to setup movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+ [Header("Set in Inspector")]
+ 
+    // Prefab for instantiating apples
+    public GameObject   applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float        speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float        leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float        chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float        secondsBetweenAppleDrops = 1f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
